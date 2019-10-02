f62e43c217d92602a2b70e1a905dcc2f831f49a5
jhonf19 <zjhon2018@gmail.com>
2019-10-02 17:40:00 -0500

    framework, icons, js y jquery is done

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php  ?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- cdn icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  
+    <!-- local icons -->
+    <!-- <link rel="stylesheet" href="css/fontawesome-free-5.11.2-web/css/all.css"> -->
+    <script src="js/jquery.js"></script>
+
+    <title>InterJota</title>
+  </head>
+  <body>
+    <h1>Hola</h1>
+    <button id="btn" type="button" name="button">hola <i class="fas fa-check"></i> </button>
+
+
+    <!-- Optional JavaScript -->
+    <script type="text/javascript">
+    $(function () {
+      $("#btn").click(function() {
+
+        alert("hola");
+      });
+
+    });
+    </script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
